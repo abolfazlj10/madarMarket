41457dbbc1c0dc05a0b6335a8bb53e8a10d69eb5
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-01 09:53:28 +0330

    add logo market and create header component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
+import Header from './components/header';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div className=''>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="bg-indigo-500 text-white h-16">
+            header
+        </div>
+    );
+}
+
+export default Header;

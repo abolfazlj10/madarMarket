8d35f4898a9d98dc8569aaeecc948a7e736f6f0c
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-01 10:04:29 +0330

    navbar postion

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Header from './components/header';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Router>
-      <div className=''>
+      <div className='max-w-screen-sm mx-auto h-screen relative'>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        <Navbar />
       </div>
     </Router>
   );
---
+++ apps/web/src/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="absolute h-16 bottom-0 w-full bg-amber-500 text-white">
+            navbar
+        </div>
+    );
+}
+
+export default Navbar;

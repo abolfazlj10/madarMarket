3c6bc23a788c7dcc734d389d852aa2f948f1447b
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-02 13:45:07 +0330

    finish offer card

--- apps/web/src/components/specialSales.tsx
+++ apps/web/src/components/specialSales.tsx
@@ -1,6 +1,22 @@
 const SpecialSales = () => {
     return (
-        <div className="border bg-[#FDF2F6] w-full h-10">test</div>
+        <div className="border bg-[#FDF2F6] border-[#FFCACA] w-full flex justify-between rounded-xl px-4 py-3">
+            <div className="flex items-center gap-2">
+                <img src="icons/fireIcon.svg" />
+                <div className="text-[#DE030A] text-sm">
+                    <div>جشنواره فروش</div>
+                    <div className="flex">
+                        <div className="font-bold">ویـــــــــــژه!</div>
+                        <img src="icons/leftArrow.svg" />
+                    </div>
+                </div>
+            </div>
+            <div className="flex gap-1 items-center text-sm text-redBg">
+                <div className="bg-[#FA2C37] rounded font-semibold text-white px-2 py-1">12</div>:
+                <div className="bg-[#FA2C37] rounded font-semibold text-white px-2 py-1">45</div>:
+                <div className="bg-[#FA2C37] rounded font-semibold text-white px-2 py-1">59</div>
+            </div>
+        </div>
     )
 }
 export default SpecialSales
--- apps/web/src/pages/home/index.tsx
+++ apps/web/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Search from "../../components/search";
 import SwiperBanner from "../../components/swiperBanner";
+import SpecialSales from '../../components/specialSales'
 
 const Home = () => {
     return (
         <div className="h-full">
             <Search />
             <SwiperBanner />
+            <SpecialSales />
         </div>
     );
 }

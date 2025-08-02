6f4a896e444c2c9fdac3741a508eac14743a755d
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-02 14:36:10 +0330

    update home view

--- apps/web/src/components/specialProductItem.tsx
+++ apps/web/src/components/specialProductItem.tsx
@@ -1,9 +1,9 @@
 const SpecialProductsItem = () => {
     return(
-        <div className="border border-[#F5F2EF] rounded-b-3xl rounded-t-xl overflow-hidden w-[200px]">
+        <div className="border border-[#F5F2EF] rounded-b-3xl rounded-t-xl space-y-1 overflow-hidden w-[200px]">
             <img src="icons/specialImag.png" className="mx-auto" />
-            <div className="">روغن زیتون بکر کریستال - 5 لیتر</div>
-            <div>
+            <div className="px-2">روغن زیتون بکر کریستال - 5 لیتر</div>
+            <div className="px-2">
                 <div className="flex gap-2 items-center">
                     <div className="text-[#787471] text-sm"><del>40,000تومان</del></div>
                     <div className="bg-[#C50F1F] text-white rounded-2xl p-1 text-xs">10%</div>
@@ -13,7 +13,7 @@ const SpecialProductsItem = () => {
                     <div className="text-[#BA400B] text-xs">تومان</div>
                 </div>
             </div>
-            <div className="bg-[#F7F7F7] border border-[#F5F2EF] rounded-3xl text-center py-2">افزودن به سبد</div>
+            <div className="bg-[#F7F7F7] border border-[#F5F2EF] rounded-3xl text-center py-2 text-[#787471] cursor-pointer">افزودن به سبد</div>
         </div>
     )
 }

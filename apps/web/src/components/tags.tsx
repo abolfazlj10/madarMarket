17d57864933867d6ede7569d02bb87ba8bcb5238
abolfazl <abolfazljamshidi101010@gmail.com>
2025-08-06 14:52:54 +0330

    handle select tag

--- apps/web/src/components/tags.tsx
+++ apps/web/src/components/tags.tsx
@@ -2,9 +2,11 @@ import { useTagsByCategory } from "../hooks/useTags";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useParams } from "react-router-dom";
 import type { tag } from "../types/type";
+import { useState } from "react";
 const Tags = () => {
     const {id} = useParams()
     const {data: tags} = useTagsByCategory(Number(id))
+    const [tagsSelected, setTagsSelected] = useState(0)
     return(
         <Swiper
             slidesPerView={4}
@@ -12,7 +14,7 @@ const Tags = () => {
             className="mySwiper cursor-pointer"
         >
             {tags?.map((item: tag,idx: number)=>(
-                <SwiperSlide key={idx} className={`bg-[#F7F7F7] !w-auto text-[#787471] rounded-2xl px-5 py-2 text-sm cursor-pointer text-center hover:border-mainColor ${idx == 0 && 'border border-mainColor text-mainColor'}`}>{item.name}</SwiperSlide>
+                <SwiperSlide onClick={() => setTagsSelected(idx)} key={idx} className={`!duration-300 bg-[#F7F7F7] !w-auto text-[#787471] rounded-2xl px-5 py-2 text-sm cursor-pointer text-center border border-mainColor/0 ${idx == tagsSelected && '!border-mainColor text-mainColor'}`}>{item.name}</SwiperSlide>
             ))}
         </Swiper>
     )
